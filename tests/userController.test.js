5eedc81a55c28227f64deda0e4f709e29dd10cce
Smit Patel <patel.smit5@northeastern.edu>
2024-06-07 13:07:38 -0400

    update in the yml file

--- tests/userController.test.js
+++ tests/userController.test.js
@@ -3,9 +3,9 @@ import app from '../index.js';
 
 describe('User API', () => {
   it('should get all users', async () => {
+    // Increase timeout to 10 seconds
     const res = await request(app).get('/api/users');
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('users');
-    expect(res.body.users).toBeInstanceOf(Array);
-  });
+  }, 10000); // Increase timeout to 10 seconds
 });
